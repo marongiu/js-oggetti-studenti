40855ba5ae05f3768b52d8d636110b0484c6331e
Andrea <andreamarongiu05@gmail.com>
2021-03-16 15:32:45 +0100

    Stampo in una table

--- main.js
+++ main.js
@@ -7,58 +7,59 @@
 //  studente inserendo nell’ordine: nome, cognome e età.
 
 
-//creo un oggetto che ha come proprietà nome,cognome e età
+$(document).ready(function() {
+  //creo un oggetto che ha come proprietà nome,cognome e età
 
-var studente = {
-  // nome
-  nome: 'andrea',
-  // cognome
-  cognome: 'marongiu',
-  // eta
-  eta: 20,
-}
-
-// stampo intero oggetto
-console.log(studente);
-// stampo tutte le proprietà dell'oggetto attraverso un ciclo for in
-for (var keys in studente) {
-  // stampo
-  console.log(studente[keys]);
-}
-
-
-// -------------------------------------------------- //
-//  Creare un array di oggetti di studenti.
-
-var studenti = [
-  {
-    // studente 1
-    nome: 'valerio',
-    cognome: 'rossi',
-    eta: 25,
-  },
-  {
-    // studente 2
-    nome: 'anna',
-    cognome: 'rossi',
-    eta: 18,
-  },
-  {
-    // studente 3
-    nome: 'marco',
-    cognome: 'esposito',
-    eta: 29,
+  var studente = {
+    // nome
+    nome: 'andrea',
+    // cognome
+    cognome: 'marongiu',
+    // eta
+    eta: 20,
   }
-]
 
-console.log(studenti);
-// creo un ciclo for per stampare le tre array
-for (var i = 0; i < studenti.length; i++) {
-  // dopo aver fatto il ciclo for creo un ciclo for in
-  for (var keys in studenti[i]) {
-    //stampo
-    console.log(studenti[i][keys]);
+  // stampo tutte le proprietà dell'oggetto attraverso un ciclo for in
+  for (var keys in studente) {
+    // stampo
+    $('#studente').append('<td>'+ studente[keys] + '</td>');
   }
-}
-// -------------------------------------------------- //
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto
+
+
+  // -------------------------------------------------- //
+  //  Creare un array di oggetti di studenti.
+
+  var studenti = [
+    {
+      // studente 1
+      nome: 'valerio',
+      cognome: 'rossi',
+      eta: 25,
+    },
+    {
+      // studente 2
+      nome: 'anna',
+      cognome: 'rossi',
+      eta: 18,
+    },
+    {
+      // studente 3
+      nome: 'marco',
+      cognome: 'esposito',
+      eta: 29,
+    }
+  ]
+
+  console.log(studenti);
+  // creo un ciclo for per stampare le tre array
+  for (var i = 0; i < studenti.length; i++) {
+    // dopo aver fatto il ciclo for creo un ciclo for in
+    for (var keys in studenti[i]) {
+      //stampo
+      console.log(studenti[i][keys]);
+    }
+  }
+  // -------------------------------------------------- //
+  // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto
+
+});
